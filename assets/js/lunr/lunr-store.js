58e639b951c78c17e0013f21fa161b4abb40b6bc
Alexsandro Zanella Arruda <nodusthc@gmail.com>
2023-02-26 14:55:11 -0300

    Added WinForms + Native AOT post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Layout: Post with Nested Table of Contents",
-        "excerpt":"Tests table of contents with multiple levels to verify indentation is readible. Enim laboris id ea elit elit deserunt Magna incididunt elit id enim nisi quis excepteur reprehenderit Lorem dolore dolore ad enim. Labore esse elit excepteur et elit dolor. Elit ut consectetur labore velit elit esse voluptate id commodo....","categories": [],
-        "tags": ["table of contents"],
-        "url": "/example-post/",
+        "title": "Building WinForms with Native AOT",
+        "excerpt":"Native AOT is incredible, we already know that, but despite microsoft not providing support for WinForms and WPF, they have already said that this is perfectly possible, although easier in Winforms than WPF, they just aren’t in the plans (they still experimented with WinForms , which is much easier, you...","categories": [],
+        "tags": [],
+        "url": "/building-winforms-nativeaot/",
         "teaser": null
       }]
